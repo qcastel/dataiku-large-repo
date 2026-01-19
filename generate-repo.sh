706ff5f8440e2cdec7835225da75265a903d59f1
Quentin Castel <quentin.castel@dataiku.com>
2026-01-19 16:57:26 +0100

    init

--- generate-repo.sh
+++ generate-repo.sh
@@ -2,27 +2,21 @@
 
 set -e
 
-REPO=big-repo
 
-mkdir "$REPO"
-cd "$REPO"
+echo "Creating fileA.csv (99mb)"
+dd if=/dev/urandom of=fileA.csv bs=1M count=99
 
-git init
+echo "Creating fileB.bin (99mb)"
+dd if=/dev/urandom of=fileB.bin bs=1M count=99
 
-echo "Creating fileA.csv (1GB)"
-dd if=/dev/urandom of=fileA.csv bs=1M count=1024
+echo "Creating fileC.toto (99mb)"
+dd if=/dev/urandom of=fileC.toto bs=1M count=99
 
-echo "Creating fileB.bin (500MB)"
-dd if=/dev/urandom of=fileB.bin bs=1M count=500
+echo "Creating fileD.csv (99mb)"
+dd if=/dev/urandom of=fileD.csv bs=1M count=99
 
-echo "Creating fileC.toto (2GB)"
-dd if=/dev/urandom of=fileC.toto bs=1M count=2048
-
-echo "Creating fileD.csv (1GB)"
-dd if=/dev/urandom of=fileD.csv bs=1M count=1024
-
-echo "Creating fileE.csv (1.5GB)"
-dd if=/dev/urandom of=fileE.csv bs=1M count=1536
+echo "Creating fileE.csv (99mb)"
+dd if=/dev/urandom of=fileE.csv bs=1M count=99
 
 git add .
 git commit -m "Add large files with specific sizes"
